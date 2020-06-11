969f3750f4b83406d33d60d147c8bbf83b6c3a5b
werliton <werliton.carlos@ceuma.br>
2020-06-11 10:52:41 -0300

    Render children inside the columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
+import { AppContainer } from './styles';
+import { Column } from './Column';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      Columns will go here
+      <Column text="Ola, aqui eh uma column"/>
+    </AppContainer>
   );
 }
 
---
+++ src/Column.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ColumnContainer, ColumnTitle } from './styles'
+
+interface ColumnProps {
+    text: string
+}
+
+export const Column = ({ text, children } : 
+    React.PropsWithChildren<ColumnProps>) => (
+    <ColumnContainer>
+        <ColumnTitle>{ text }</ColumnTitle>
+        {children}
+    </ColumnContainer>
+)
